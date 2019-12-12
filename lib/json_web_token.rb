f2731594ad1b52f7486e5c2e467053befa2ae5bc
Amy Nguyen <amy.nguyen730@gmail.com>
2019-12-12 16:27:54 -0700

    adds env yml to config and uses ENV in json_web_token

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
     class << self
       def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
-        JWT.encode(payload, "secret")
+        JWT.encode(payload, ENV["USERNAME", ENV["PASSWORD"]])
       end
    
       def decode(token)
-        body = JWT.decode(token, "secret")[0]
+        body = JWT.decode(token, ENV["USERNAME", ENV["PASSWORD"])[0]
         HashWithIndifferentAccess.new body
       rescue
         nil
